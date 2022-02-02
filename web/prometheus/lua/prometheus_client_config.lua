0d9a98deabc858d837e45b2dff19cc9158835465
Esteban Abadia <56838254+abadiaesteban@users.noreply.github.com>
2022-02-02 21:43:28 +0100

    Prometheus

---
+++ web/prometheus/lua/prometheus_client_config.lua
@@ -0,0 +1,77 @@
+--[[ Client Config ]]--
+
+	Prometheus.WebsiteUrl = "http://garryhost.com/" -- Your websites URL that will open when they click on the donation button in the menu, has to start with http:// or https://
+
+	Prometheus.Notify.Header = "garry's host"
+
+	Prometheus.Notify.BGColor = Color(33, 35, 35, 255)
+
+	Prometheus.Notify.TitleColor = Color(193, 0, 0, 255)
+	Prometheus.Notify.TitleBGColor = Color(26, 29, 29, 255)
+
+	Prometheus.Notify.TextColor = Color(119, 119, 119, 255)
+
+	Prometheus.Notify.CloseColor = Color(26, 29, 29, 255)
+	Prometheus.Notify.CloseTextColor = Color(200, 200, 200, 255)
+
+	Prometheus.Notify.ShowTime = 5 -- Time in seconds, how long should the notification stay on screen
+
+
+	Prometheus.Menu.Header = "garry's host"
+
+	Prometheus.Menu.BGColor = Color(33, 35, 35, 255)
+
+	Prometheus.Menu.TabTextColor = Color(230, 230, 230, 255)
+	Prometheus.Menu.TabColor = Color(26, 29, 29, 255)
+	Prometheus.Menu.TabColorSelected = Color(51, 51, 51, 255)
+
+	Prometheus.Menu.TitleColor = Color(193, 0, 0, 255)
+	Prometheus.Menu.TitleBGColor = Color(26, 29, 29, 255)
+
+	Prometheus.Menu.PackageTitleBGColor = Color(19, 21, 21, 255)
+	Prometheus.Menu.PackageTitleTextColor = Color(220, 220, 220, 255)
+	Prometheus.Menu.PackageBGColor = Color(26, 29, 29, 255)
+	Prometheus.Menu.PackageLineBGColor1 = Color(51, 51, 51, 255)
+	Prometheus.Menu.PackageLineBGColor2 = Color(26, 29, 29, 255)
+	Prometheus.Menu.PackageLineTextColor = Color(230, 230, 230, 255)
+
+	Prometheus.Menu.AdminTitleBGColor = Color(19, 21, 21, 255)
+	Prometheus.Menu.AdminTitleTextColor = Color(220, 220, 220, 255)
+	Prometheus.Menu.AdminBGColor = Color(26, 29, 29, 255)
+	Prometheus.Menu.AdminLineBGColorError = Color(193, 0, 0)
+	Prometheus.Menu.AdminLineBGColorInfo = Color(0, 102, 204)
+	Prometheus.Menu.AdminLineTextColor = Color(230, 230, 230, 255)
+
+	Prometheus.Menu.CloseColor = Color(26, 29, 29, 255)
+	Prometheus.Menu.CloseHoverColor =Color(19, 21, 21, 255)
+	Prometheus.Menu.CloseTextColor = Color(200, 200, 200, 255)
+
+	Prometheus.Menu.ScrollBGColor = Color(57, 57, 57, 255)
+	Prometheus.Menu.ScrollUpColor = Color(19, 21, 21, 255)
+	Prometheus.Menu.ScrollDownColor = Color(19, 21, 21, 255)
+	Prometheus.Menu.ScrollGripColor = Color(26, 29, 29, 255)
+
+	Prometheus.Menu.HoverTextColor = Color(220, 220, 220, 255)
+	Prometheus.Menu.HoverTextBGColor = Color(57, 57, 57, 200)
+
+	Prometheus.Lang.Packages = "Offre"
+	Prometheus.Lang.Donate = "Faire un don"
+	Prometheus.Lang.Admin = "Admin"
+	Prometheus.Lang.Package = "a acheter une offre"
+	Prometheus.Lang.ExpireDate = "Date d'expiration"
+	Prometheus.Lang.State = "État"
+	Prometheus.Lang.StateActive = "Actif"
+	Prometheus.Lang.StateInactive = "Innactif"
+	Prometheus.Lang.ExpiresNever = "Jamais"
+
+	Prometheus.Lang.Text = "Text"
+	Prometheus.Lang.Time = "Temps"
+
+	Prometheus.Lang.OnCooldown = "Vous êtes en période de récupération pour empêcher l'activité de la base de données de spam, réessayez dans quelques instants."
+	Prometheus.Lang.DBFailed = "Le serveur n'a pas réussi à se connecter à la base de données ! Veuillez contacter le fondateur."
+
+	Prometheus.Lang[1] = "Vous venez de revérifier votre classement déjà."
+	Prometheus.Lang[2] = "Aucune action de classement active trouvée."
+	Prometheus.Lang[3] = "Vous a réattribué votre dernier rang."
+
+--[[ End of Client Config ]]--
